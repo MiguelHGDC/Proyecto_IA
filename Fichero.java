129b8634ea374bb1c3958806fa2b90878850cd45
Miguel <73239423+MiguelHGDC@users.noreply.github.com>
2020-12-09 17:28:48 +0100

    Update Fichero.java

--- Fichero.java
+++ Fichero.java
@@ -13,80 +13,73 @@ public class Fichero {
         String nuevalinea2 = "";
         String[] partes;
         String[] aux;
+        int tipo = 0; //1->Lugar, 2->Personaje, 3->Objeto
 
         try {
-            FileInputStream fichero = new FileInputStream("C:\\Users\\mhere\\OneDrive\\Escritorio\\U-TAD\\Curso2\\Programacion orientada a objetos\\08-POO\\Trabajos\\ProyectoJava\\src\\Prueba_lectura.txt");
+            FileInputStream fichero = new FileInputStream("C:\\Users\\mhere\\OneDrive\\Escritorio\\U-TAD\\Curso2\\Programacion orientada a objetos\\08-POO\\Proyecto_Java\\Prueba_lectura.txt");
             Scanner src = new Scanner(fichero);
 
-            while(src.hasNextLine()){
-                linetxt = src.nextLine();
+            while(src.hasNextLine()) {              
                 //System.out.println(linetxt);
+            	linetxt = src.nextLine(); 
+            	
+            	if(linetxt.contains("<")) {
+            		
+            		if(linetxt.contains("<Localizaciones>")) {
+            			tipo = 1;
+            		}
+            		else if(linetxt.contains("<Personajes>")) {
+            			tipo = 2;
+            		}
+            		else {
+            			tipo = 3;
+            		}
+            		
+            	}
+            	else {
+            		if(tipo == 1) {
+            			nuevalinea = linetxt.replace("(", "-").replace(")", "").replace(",","-").trim();
+                        
+                        partes = nuevalinea.split("-");
+                        
+                        nuevalinea2 = linetxt.replace(partes[0],"").replace("(", "").replace(")", "").replace(",","-").trim();
+                        
+                       // System.out.println("\n\n NUEVA LINEA 2 :"+nuevalinea2);
+                        aux = nuevalinea2.split("-");
 
-                if(linetxt.contains("<Localizaciones>")){ //Parte del texto donde se encuentran las localizaciones.
-                    while(true){ //Captura de todas las localizaciones que hay
-                        linetxt = src.nextLine();
-                        if(linetxt.contains("</Localizaciones>")){
-                            break;
-                        }else{
-                            nuevalinea = linetxt.replace("(", "-").replace(")", "").replace(",","-").trim();
-                            
-                            partes = nuevalinea.split("-");
-                            
-                            nuevalinea2 = linetxt.replace(partes[0],"").replace("(", "").replace(")", "").replace(",","-").trim();
-                            
-                           // System.out.println("\n\n NUEVA LINEA 2 :"+nuevalinea2);
-                            aux = nuevalinea2.split("-");
+                        Lugar lugar = new Lugar(partes[0],aux);
+                        lugares.add(lugar);
+            		}
+            		else if(tipo == 2) {
+            			
+            			nuevalinea = linetxt.replace("(", "-").replace(")", "").replace(",","-").trim();
+                        
+                        partes = nuevalinea.split("-");
+                        
+                        nuevalinea2 = linetxt.replace(partes[0],"").replace("(", "").replace(")", "").replace(",","-").trim();
+                        
+                        //System.out.println("\n\n NUEVA LINEA 2 :"+nuevalinea2);
+                        aux = nuevalinea2.split("-");
 
-                            Lugar lugar = new Lugar(partes[0],aux);
-                            lugares.add(lugar);
+                        Personaje personaje = new Personaje(partes[0],aux[0]);
+                        personajes.add(personaje);
+            		}
+            		else {
+            			nuevalinea = linetxt.replace("(", "-").replace(")", "").replace(",","-").trim();
+                        
+                        partes = nuevalinea.split("-");
+                        
+                        nuevalinea2 = linetxt.replace(partes[0],"").replace("(", "").replace(")", "").replace(",","-").trim();
+                        
+                        //System.out.println("\n\n NUEVA LINEA 2 :"+nuevalinea2);
+                        aux = nuevalinea2.split("-");
 
-                        }
-                    }
-                }
-                
-                if(linetxt.contains("<Personajes>")){ //Parte del texto donde se encuentran las localizaciones.
-                    while(true){ //Captura de todas las localizaciones que hay
-                        linetxt = src.nextLine();
-                        if(linetxt.contains("</Personajes>")){
-                            break;
-                        }else{
-                            nuevalinea = linetxt.replace("(", "-").replace(")", "").replace(",","-").trim();
-                            
-                            partes = nuevalinea.split("-");
-                            
-                            nuevalinea2 = linetxt.replace(partes[0],"").replace("(", "").replace(")", "").replace(",","-").trim();
-                            
-                            //System.out.println("\n\n NUEVA LINEA 2 :"+nuevalinea2);
-                            aux = nuevalinea2.split("-");
-
-                            Personaje personaje = new Personaje(partes[0],aux[0]);
-                            personajes.add(personaje);
-
-                        }
-                    }
-                } 
-                if(linetxt.contains("<Objetos>")){ //Parte del texto donde se encuentran las localizaciones.
-                    while(true){ //Captura de todas las localizaciones que hay
-                        linetxt = src.nextLine();
-                        if(linetxt.contains("</Objetos>")){
-                            break;
-                        }else{
-                            nuevalinea = linetxt.replace("(", "-").replace(")", "").replace(",","-").trim();
-                            
-                            partes = nuevalinea.split("-");
-                            
-                            nuevalinea2 = linetxt.replace(partes[0],"").replace("(", "").replace(")", "").replace(",","-").trim();
-                            
-                            //System.out.println("\n\n NUEVA LINEA 2 :"+nuevalinea2);
-                            aux = nuevalinea2.split("-");
-
-                            Objeto objeto = new Objeto(partes[0],aux[0]);
-                            objetos.add(objeto);
-
-                        }
-                    }
-                } 
-            }
+                        Objeto objeto = new Objeto(partes[0],aux[0]);
+                        objetos.add(objeto);
+            		}
+            	}
+            }	
+            
             System.out.println("[     LUGARES     ]\n");
             
             for(Lugar nini:lugares) {
@@ -111,7 +104,9 @@ public class Fichero {
 
             src.close(); //Cerramos el scanner al fichero.
             fichero.close(); //Cerramos el fichero.
-        } catch (Exception e) { //Levanto una excepcion si no se ha podido leer el fichero
+        } 
+        
+        catch (Exception e) { //Levanto una excepcion si no se ha podido leer el fichero
             e.printStackTrace();
         }
 	}
